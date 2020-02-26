b45b4e5b848405190a18d95e1abe7adb3c28235b
Juanjo Gallardo <jjgc.92@hotmail.com>
2020-02-26 12:59:52 +0100

    Added Common Features

---
+++ Src/Core/Application/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Trip.Application.Common.Behaviours;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestPerformanceBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehavior<,>));
+            return services;
+        }
+    }
+}
---
+++ Src/Core/Common/IDateTime.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trip.Common
+{
+    public interface IDateTime
+    {
+        DateTime Now { get; }
+    }
+}
